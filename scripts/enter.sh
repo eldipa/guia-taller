ba38c08c8a2eca9b74b1abdd0c9b3596d6c73bef
Martin Di Paola <martinp.dipaola@gmail.com>
2021-08-08 02:11:11 +0000

    Hack: make sure that we have python env activated to have a consistent PATH

--- scripts/enter.sh
+++ scripts/enter.sh
@@ -5,6 +5,15 @@ set -o pipefail
 
 . scripts/x/run_in_docker.sh
 
+# This is a hack but we must enforce a consistent environment
+# so we can export to the docker the same environment and it will
+# not change from run to run. This is critical to make TUP to not
+# trigger a full recompilation on an environment change.
+if [[ "$PATH" != *"py3env"* ]]; then
+    echo "It seems that you are *not* in a python environment. You need to enter there first."
+    exit 1
+fi
+
 export INDOCKER=1
 EXTRA="-it --env INDOCKER --env PATH -h builder"
 run_in_docker bash
